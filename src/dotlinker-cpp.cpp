56bfc0c8116dc8bcb16052fc5ea791bb19700695
Jacob Killelea <Jkillelea@protonmail.ch>
2016-12-10 18:31:10 -0700

    refactor for arbitrary numbers of arguments, more error checking

--- src/dotlinker-cpp.cpp
+++ src/dotlinker-cpp.cpp
@@ -2,35 +2,42 @@
 // Copyright Jacob Killelea <jkillelea@protonmail.ch>
 #include <cstdlib>   // exit
 #include <unistd.h>  // symlink
-#include <iostream>  // cout, cerr
-#include <fstream>   // check file existence
-#include "dotfile.h" // class DotFile, type string = std::string
+#include <iostream>  // cerr
+#include <fstream>   // ifstream - check file existence
+#include "dotfile.h" // class DotFile, string
 using namespace std;
 
 void err(string text, int code = 1); // prototype
 
 int main(int argc, const char* argv[]) {
+  string path;
 
   if (argc < 2) { // didn't supply any options
     err("[ERROR] need to give a filename");
   }
 
-  for (int i = 1; i < argc; i++) { // each supplied arg
-    string path = argv[i];
-    if (path.substr(0, 1) == "-") continue; // skip configuration options
+  for (int i = 1; i < argc; i++) {
 
+    try {
+      path = argv[i];
+    } catch (std::logic_error& e) { // failed to assign (no opts given)
+      std::cerr << "[ERROR] Error incountered parsing command line options" << std::endl;
+      std::cerr << e.what() << std::endl;
+      return 1;
+    }
+
+    if (path.substr(0, 1) == "-") continue; // skip configuration options
     DotFile dotfile(path); // create object
 
     if (ifstream(dotfile.dotfile_path.c_str()))
       err("[ERROR] File already exists in linking location");
 
     if (dotfile.exists) { // good file given
-      symlink(dotfile.absolute_path.c_str(), dotfile.dotfile_path.c_str());
-      cout << "linked " << dotfile.absolute_path << " to " << dotfile.dotfile_path << endl;
-      return 0;
-    } else {
-      err("Need to be given a real filepath");
-    }
+        symlink(dotfile.absolute_path.c_str(), dotfile.dotfile_path.c_str());
+        std::cerr << ("linked " + dotfile.absolute_path + " to " + dotfile.dotfile_path) << std::endl;
+      }
+    else
+      err(("Need to be given a real filepath instead of " + dotfile.absolute_path));
   }
   return 0;
 }
