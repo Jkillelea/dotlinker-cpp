579c536905790d614b4000b68fc49126e2c5b30c
Jacob Killelea <jkillelea@protonmail.ch>
2017-01-07 21:31:43 -0800

    messing around a little

--- src/test.cpp
+++ src/test.cpp
@@ -1,28 +1,31 @@
 // This code is part of dotlinker-cpp
 // Copyright Jacob Killelea <jkillelea@protonmail.ch>
 // this script is just random testing crap, not to be used with the expectation of fucntionality
-#include <iostream> // cout, cerr
+#include <iostream>  // cout, cerr
+#include <unistd.h>  // sleep
 #include "dotfile.h" // classdef, filesystem
 using namespace std;
 
 int main(int argc, char const *argv[]) {
+
+  sleep(1);
+
   try {
     // fs::path p(argv[1]); // different methods of instantiating a dotfile
     // DotFile dotfile(p);
     DotFile dotfile(argv[1]);
-
-    fs::path home(getenv("HOME"));
-
-    cout << "Exists       : "      << (dotfile.exists? "true":"false") << endl;
-    cout << "Absolute     : "      << dotfile.absolute_path            << endl;
-    cout << "Basename     : "      << dotfile.basename                 << endl;
-    cout << "Dotted       : "      << dotfile.dot()                    << endl;
-    cout << "Undotted     : "      << dotfile.undot()                  << endl;
-    cout << "Home         : "      << fs::absolute(home).c_str() << endl;
-    cout << "Dotfile path : "      << dotfile.dotfile_path             << endl;
   }
   catch (exception&) {
     cerr << "I dont know what went wrong but you did it." << endl;
   }
+  fs::path home(getenv("HOME"));
+
+  cout << "Exists       : "      << (dotfile.exists? "true":"false") << endl;
+  cout << "Absolute     : "      << dotfile.absolute_path            << endl;
+  cout << "Basename     : "      << dotfile.basename                 << endl;
+  cout << "Dotted       : "      << dotfile.dot()                    << endl;
+  cout << "Undotted     : "      << dotfile.undot()                  << endl;
+  cout << "Home         : "      << fs::absolute(home).c_str() << endl;
+  cout << "Dotfile path : "      << dotfile.dotfile_path             << endl;
   return 0;
 }
